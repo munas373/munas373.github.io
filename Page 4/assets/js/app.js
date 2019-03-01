be9af4de79f7175928e1fe27571e9ed40bbbcb44
KARISSA <munas373@newschool.edu>
2019-03-01 13:59:31 -0500

    More updates

--- Page 4/assets/js/app.js
+++ Page 4/assets/js/app.js
@@ -1,3 +1,22 @@
+/**
+  Javascript isn't required for this effect,
+  I just added a quick piece of jQuery code
+  to simulate a hover for the animated preview
+  on CodePen.io.
+*/
+
+// simulation code, ignore it.
+$(function() {
+  setTimeout(function() {
+    $("article.highlight").toggleClass("hover");
+    setTimeout(function() {
+      $("article.highlight").toggleClass("hover");
+    }, 2000);
+  }, 1000);
+});
+
+
+
 (function emojiCursor() {
   
   var possibleEmoji = ["art", "art", "art", "art"]
